4762cca2e6d954e66624c9bddd7ecfef2010c6d5
Saurav Pahadia <saurav@augment.co>
2021-01-29 22:16:36 -0700

    Auth

--- eloleague/pages/_app.tsx
+++ eloleague/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import {Provider} from "next-auth/client";
+import {AppProps} from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>);
+
 }
 
 export default MyApp
