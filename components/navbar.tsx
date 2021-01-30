c08c8c7ae08e4cf514adf4b5afb9f42f1d70ae68
wwsalmon <wwsamson@yahoo.com>
2021-01-29 22:25:50 -0700

    make base navbar and landing page

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div className="bg-black w-full">
+            <div className="max-w-4xl mx-auto px-4 h-16 flex items-center sticky top-0">
+                <p className="font-bold text-xl text-white">EloLeague</p>
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
-  return (
-    <p>Hello world</p>
-  )
+    return (
+        <div className="bg-black w-full text-white">
+            <div className="max-w-4xl mx-auto px-4 py-16">
+                <div className="text-center">
+                    <h1 className="text-6xl font-semibold el-font-display uppercase mb-4">Know who's actually better</h1>
+                    <p className="text-xl">Elo rankings and game records for your league, club, or house</p>
+                </div>
+            </div>
+        </div>
+    )
 }
