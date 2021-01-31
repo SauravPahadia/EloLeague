474e716f78e25f02d8aa1e1dadb708a0ad851d25
Saurav Pahadia <saurav@augment.co>
2021-01-30 22:15:42 -0700

    WIP

--- pages/api/league/new.ts
+++ pages/api/league/new.ts
@@ -43,7 +43,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             }
         ]);
 
-    console.log(data);
 
     return res.status(200).json({league: data});
 }
